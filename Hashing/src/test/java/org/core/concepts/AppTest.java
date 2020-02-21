a58cd8eceb3e2dde7875ae682d3561ae402ddcde
Veeraraghavan Narasimhan <veeraraghavan1989@gmail.com>
2020-02-21 11:24:59 -0800

    Add hashing implmentation

---
+++ Hashing/src/test/java/org/core/concepts/AppTest.java
@@ -0,0 +1,20 @@
+package org.core.concepts;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
